8d95b3317d318d550784bb510682ec4ddc39735c
nikx <87845569+nikx-gupta@users.noreply.github.com>
2021-09-24 20:59:49 +0530

    init

---
+++ simple/src/test/java/com/az/apigateway/apibackend/ApibackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.apigateway.apibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
